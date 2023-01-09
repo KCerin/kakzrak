33c33e86c6afd4afe6bdf098a66af73d79ea3ac9
edis1410 <edisfejzoski@gmail.com>
2023-01-09 13:13:44 +0100

    popup in styli

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,24 @@
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import Popup from 'reactjs-popup';
+
+
+// je treba installat reactjs-popup
 
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>kakZrak Slovenija</h1>
             <div className="links">
-                <InfoOutlinedIcon></InfoOutlinedIcon>
+            <Popup arrow="false"
+                trigger={open => (
+                    <InfoOutlinedIcon>Trigger - {open ? 'Opened' : 'Closed'}</InfoOutlinedIcon>
+                )}
+                offsetX="10px"
+                position="left center"
+                closeOnDocumentClick
+                >
+                <p> KakZrak je stran, ki prikaže kakovost zraka v Sloveniji skozi čas </p>
+            </Popup>
             </div>
         </nav>
      );
